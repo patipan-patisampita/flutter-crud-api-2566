a54a4f0422b6c16ef40673a4c565dc360189ed4a
patipan-patisampita <patipan-patisampita@gmail.com>
2024-02-12 11:54:47 +0700

    update

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -46,7 +46,7 @@ class _HomePageState extends State<HomePage> {
         backgroundColor: Colors.purple,
         title: Text(widget.title),
       ),
-      body: ListView.builder(
+      body:_isLoading ? const CircularProgressIndicator(): ListView.builder(
         itemCount: _listdata.length,
         itemBuilder: (context, index) {
           return Card(
